ebb25110872ab7c2f07805047a4919fc9c507b8c
Maxence995 <45846956+Maxence995@users.noreply.github.com>
2021-01-28 11:22:42 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,8 @@
+
+function calculate() {
+    var x = document.getElementById("input-item").value;
+    if(x){
+        document.getElementById("output").innerHTML = x + " 100 EUR";
+    }
+    
+  }
